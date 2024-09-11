1ec1869e3a8fd562c99c431dfb879d7a5e6212df
pradeep2325 <pkmaligarjun@gmail.com>
2024-09-11 14:31:20 +0530

    recent code

---
+++ src/test/java/org/test007/Pradeep001.java
@@ -0,0 +1,34 @@
+package org.test007;
+
+public class Pradeep001 {
+	public static void main(String[] args) {
+		System.out.println("pradeep");
+		System.out.println("kumar");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
